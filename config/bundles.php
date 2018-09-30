14bfff53011a705841c6dfd5cd783129237a86ba
Alexandr Zolotukhin <supersmile2009@gmail.com>
2018-09-30 18:30:39 +0300

    Installed and configured additional dependencies
    FOSRestBundle, FrameworkExtraBundle, Symfony Serializer, Symfony Expression Language

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,6 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
